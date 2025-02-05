1091ead3f26f7391f50fec679fd3bf865834d420
Oğulcan KAÇAR <63792003+OgulcanKacarr@users.noreply.github.com>
2025-02-05 05:44:52 +0300

    Update AppStrings.dart

--- lib/constants/AppStrings.dart
+++ lib/constants/AppStrings.dart
@@ -5,6 +5,21 @@ class AppStrings {
   static const String save = "Kaydet";
   static const String savedSuccessful = "Kayıt başarılı";
 
+  //form
+  static const String email = "Email";
+  static const String password = "Şifre";
+  static const String login = "Giriş yap";
+  static const String or = "Ya da";
+  static const String register = "Kayıt ol";
+  static const String rememberMe = "Beni hatırla";
+
+
+  //errors
+  static const String emailNotEmpty = "Email boş olamaz";
+  static const String passwordNotEmpty = "Şifre boş olamaz";
+  static const String infoNotEmpty = "Bilgileri doldurun";
+
+
 
 
 }

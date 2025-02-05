f389465868305a9c8531709247047ec4aff9891f
Oğulcan KAÇAR <63792003+OgulcanKacarr@users.noreply.github.com>
2025-02-05 05:45:31 +0300

    Update LoginPageViewModel.dart

--- lib/viewModel/auth/LoginPageViewModel.dart
+++ lib/viewModel/auth/LoginPageViewModel.dart
@@ -1,5 +1,31 @@
+import 'package:base_app/constants/AppStrings.dart';
+import 'package:base_app/constants/ConstMethods.dart';
+import 'package:base_app/services/AbstractClassForServices/AbstractAuthService.dart';
+import 'package:base_app/services/FirebaseAuthService.dart';
 import 'package:flutter/cupertino.dart';
 
-class LoginPageViewModel extends ChangeNotifier{
+class LoginPageViewModel extends ChangeNotifier {
+  final AbstractAuthService _authService = FirebaseAuthService();
 
+  bool _isRememberMe = false;
+
+  get getRememberMe => _isRememberMe;
+
+  void setRememberMe(bool value) {
+    _isRememberMe = value;
+    notifyListeners();
+  }
+
+  Future<void> login(
+      BuildContext context, String email, String password) async {
+    if (email.isEmpty && password.isEmpty) {
+      ConstMethods.showSnackBar(context, AppStrings.infoNotEmpty);
+    } else if (email.isEmpty) {
+      ConstMethods.showSnackBar(context, AppStrings.emailNotEmpty);
+    } else if (password.isEmpty) {
+      ConstMethods.showSnackBar(context, AppStrings.passwordNotEmpty);
+    } else {
+      _authService.signIn(email, password);
+    }
+  }
 }
